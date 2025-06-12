87837d92d58a4d72ff44fb9246cf49ad969ef93f
hafizulislamhfz <hafizulislamhfz@gmail.com>
2025-06-12 14:23:40 +0600

    add: log out, fix: dir struct

--- routes/web/auth.php
+++ routes/web/auth.php
@@ -1,6 +1,9 @@
 <?php
 
-use App\Http\Controllers\Auth\GoogleAuthController;
+use App\Http\Controllers\Web\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Web\Auth\GoogleAuthController;
 
 Route::get('/auth/google', [GoogleAuthController::class, 'redirect'])->name('login');
 Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback']);
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
